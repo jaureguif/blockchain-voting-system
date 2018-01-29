99084339043f0a26bd63c71e31c4a3ba3ddecd7b
Dionicio Rodriguez <Dionicio_Rodriguez@epam.com>
2018-01-29 17:38:49 -0600

    reverting changes related with CommandLineRunner

--- src/main/java/com/epam/asset/tracking/Application.java
+++ src/main/java/com/epam/asset/tracking/Application.java
@@ -1,11 +1,18 @@
 package com.epam.asset.tracking;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -14,8 +21,25 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @Configuration 
 public class Application {
 
+	Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			log.info("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				log.info(beanName);
+			}
+
+		};
 	}
 }
