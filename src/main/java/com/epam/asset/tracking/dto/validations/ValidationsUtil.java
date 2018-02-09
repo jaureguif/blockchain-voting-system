1791fe3352903a28d8a14baf27d45d5c5bb57baf
jaureguif <jaureguif@users.noreply.github.com>
2018-02-09 11:25:56 -0600

    Update ValidationsUtil.java

--- src/main/java/com/epam/asset/tracking/dto/validations/ValidationsUtil.java
+++ src/main/java/com/epam/asset/tracking/dto/validations/ValidationsUtil.java
@@ -1,20 +1,19 @@
 package com.epam.asset.tracking.dto.validations;
 
-public interface ValidationsUtil {
+public final class ValidationsUtil {
 	
-	String LETTER = "[a-zA-ZáéíóúÁÉĪÓÚ\\u00f1\\u00d1]";
+	private static final String LETTER = "[a-zA-ZáéíóúÁÉĪÓÚ\\u00f1\\u00d1]";
 	
-	String LETTER_OR_NUMBER = "[a-zA-Z0-9áéíóúÁÉĪÓÚ\\u00f1\\u00d1]";
+	private static final String LETTER_OR_NUMBER = "[a-zA-Z0-9áéíóúÁÉĪÓÚ\\u00f1\\u00d1]";
 	
-	String LETTERS_ONLY = "^" + LETTER + "*$";
+	public static final String LETTERS_ONLY = "^" + LETTER + "*$";
 	
-	String LETTERS_WITH_SPACE = "^(" + LETTER +"+[\\s]?)*" + LETTER + "$";
+	public static final String LETTERS_WITH_SPACE = "^(" + LETTER +"+[\\s]?)*" + LETTER + "$";
 	
-	String LETTERS_AND_NUMBERS = "^" + LETTER_OR_NUMBER + "*$";
+	public static final String LETTERS_AND_NUMBERS = "^" + LETTER_OR_NUMBER + "*$";
 	
-	String LETTERS_AND_NUMBERS_WITH_SPACE = "^(" + LETTER_OR_NUMBER +"+[\\s]?)*" + LETTER_OR_NUMBER + "$";
+	public static final String LETTERS_AND_NUMBERS_WITH_SPACE = "^(" + LETTER_OR_NUMBER +"+[\\s]?)*" + LETTER_OR_NUMBER + "$";
 	
-	String LETTERS_AND_NUMBERS_WITH_SPACE_COMMA_AND_PERIOD = "^(" + LETTER_OR_NUMBER + "+[,|[.]{0,3}]?[\\s]?)*" + LETTER_OR_NUMBER + "+[.]{0,3}$";
-	
-	
+	public static final String LETTERS_AND_NUMBERS_WITH_SPACE_COMMA_AND_PERIOD = "^(" + LETTER_OR_NUMBER + "+[,|[.]{0,3}]?[\\s]?)*" + LETTER_OR_NUMBER + "+[.]{0,3}$";
+		
 }
