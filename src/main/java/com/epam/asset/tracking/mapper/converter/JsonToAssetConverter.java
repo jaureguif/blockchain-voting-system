89e17f91a1a89036eef814411933e3e31d5341b6
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-12 10:25:03 -0600

    Converts JSON String its match Java Object (Asset) using Jackson Object Mapper

---
+++ src/main/java/com/epam/asset/tracking/mapper/converter/JsonToAssetConverter.java
@@ -0,0 +1,36 @@
+package com.epam.asset.tracking.mapper.converter;
+
+import com.epam.asset.tracking.domain.Asset;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Created on 1/25/2018.
+ */
+@Component
+public class JsonToAssetConverter {
+
+    /**
+     * Created by Miguel Monraz on 12/02/2018
+     * Converts a given JSON String into an Java Object
+     * @param entity
+     * @return Asset
+     *
+     **/
+
+    public Asset convert(String entity) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Asset asset = null;
+        try {
+            asset = objectMapper.readValue(entity, Asset.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return asset;
+    }
+}
