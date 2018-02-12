ae4f527284f8488a91ed9f3ec737587c072dc471
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-12 12:58:02 -0600

    Change test to reflect the new JSON format

--- src/test/java/com/epam/asset/tracking/mapper/converter/JsonToAssetConverterTest.java
+++ src/test/java/com/epam/asset/tracking/mapper/converter/JsonToAssetConverterTest.java
@@ -10,7 +10,7 @@ public class JsonToAssetConverterTest {
     @Test
     public void convertJSONStringToAsset(){
 
-        String jsonAsset = "{\"id\": \"1\",\"name\": \"vehicle\", \"description\": \"figo 2018\"}";
+        String jsonAsset = "{\"uuid\": \"9d40ee4e-bf1e-4f74-8237-c5e9b6e8f6d3\", \"serialNumber\": \"3VW1W21KIBM312176\", \"assetType\": \"VEHICLE\", \"ownerName\": \"Jhonn Doe\", \"description\": \"2011 VW JETTA STYLE ACTIVE MANUAL TRANSMISION. SIDE AIRBAGS PACKAGE, COLOR: WHITE CANDY INTERIOR COLOR: BLACK FABRIC . ENGINE: 2.5L FIVE CYLINDERS ENGINE NUMBER: CCC094323 MADE IN: MEXICO BUYER NAME: JHONN DOE ADDRESS: 123 ABBY ROAD, THE DOMAIN. AUTIN TEXAS, USA. SELLER NAME: RAY REDDINGTON\"}";
 
         JsonToAssetConverter converter = new JsonToAssetConverter();
 
@@ -21,9 +21,10 @@ public class JsonToAssetConverterTest {
         asset  = converter.convert(jsonAsset);
 
         //Check if the values from the Asset object created matched the on the JSON String
-        assertEquals(asset.getId(), "1");
-        assertEquals(asset.getName(), "vehicle");
-        assertEquals(asset.getDescription(), "figo 2018");
+        assertEquals(asset.getUuid(), "9d40ee4e-bf1e-4f74-8237-c5e9b6e8f6d3");
+        assertEquals(asset.getSerialNumber(), "3VW1W21KIBM312176");
+        assertEquals(asset.getAssetType(), "VEHICLE");
+        assertEquals(asset.getDescription(),"2011 VW JETTA STYLE ACTIVE MANUAL TRANSMISION. SIDE AIRBAGS PACKAGE, COLOR: WHITE CANDY INTERIOR COLOR: BLACK FABRIC . ENGINE: 2.5L FIVE CYLINDERS ENGINE NUMBER: CCC094323 MADE IN: MEXICO BUYER NAME: JHONN DOE ADDRESS: 123 ABBY ROAD, THE DOMAIN. AUTIN TEXAS, USA. SELLER NAME: RAY REDDINGTON");
 
 
     }
