e05143273019b5b7a92296c04b935bf4dc8e1ba6
dan-zx <danyboy_shaggy@hotmail.com>
2018-02-12 12:25:39 -0600

    Modified Go chain code to use struct+JSON in ledger storage

--- src/test/fixture/sdkintegration/gocc/sample1/src/github.com/example_cc/example_cc.go
+++ src/test/fixture/sdkintegration/gocc/sample1/src/github.com/example_cc/example_cc.go
@@ -17,8 +17,8 @@ limitations under the License.
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
@@ -28,54 +28,38 @@ import (
 type SimpleChaincode struct {
 }
 
+type Asset struct {
+	UUID         string `json:"uuid"`
+	SerialNumber string `json:"serialNumber"`
+	AssetType    string `json:"assetType"`
+	OwnerName    string `json:"ownerName"`
+	Description  string `json:"description"`
+}
+
 // Init initializes the chaincode state
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("########### example_cc Init ###########")
-	_, args := stub.GetFunctionAndParameters()
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
+	fmt.Println("########### Init ###########")
+	asset := Asset{
+		UUID:         "14a12ef0-9409-4872-9341-9ab003059ce9",
+		SerialNumber: "3SZ1W278EJ8",
+		AssetType:    "VEHICLE",
+		OwnerName:    "THOMAS ALVA EDISON",
+		Description:  "1908 FORD MODEL T\nCOLOR: ANYONE, AS LONG AS IT IS BLACK\nINTERIORS COLOR: BROWN LEATHER.\nENGINE: 2.9 L FOUR CYLINDERS\nENGINE NUMBER: 000015\nMADE IN: UNITED STATES\nBUYER NAME: THOMAS EDISON\nADDRESS: 345 Llewellyn Park, New Jersey, United States, USA.\nSELLER NAME: HENRY FORD",
 	}
+	jsonBlob, _ := json.Marshal(asset)
+	err := stub.PutState("1", jsonBlob)
 
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	if transientMap, err := stub.GetTransient(); err == nil {
-		if transientData, ok := transientMap["result"]; ok {
-			return shim.Success(transientData)
-		}
-	}
 	return shim.Success(nil)
 
 }
 
 // Invoke makes payment of X units from A to B
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("########### example_cc Invoke ###########")
+	fmt.Println("########### Invoke ###########")
 	function, args := stub.GetFunctionAndParameters()
 
 	if function != "invoke" {
@@ -86,128 +70,44 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return shim.Error("Incorrect number of arguments. Expecting at least 2")
 	}
 
-	if args[0] == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	}
-
-	if args[0] == "query" {
-		// queries an entity state
+	fn := args[0]
+	switch fn {
+	case "create":
+		return shim.Error("Not yet implemented")
+	case "query":
 		return t.query(stub, args)
+	case "update":
+		return shim.Error("Not yet implemented")
+	case "delete":
+		return shim.Error("Not yet implemented")
+	default:
+		return shim.Error("Unknown action, check the first argument, must be one of 'create', 'query', 'update' or 'delete'")
 	}
-	if args[0] == "move" {
-		// Deletes an entity from its state
-		return t.move(stub, args)
-	}
-	return shim.Error("Unknown action, check the first argument, must be one of 'delete', 'query', or 'move'")
 }
 
-func (t *SimpleChaincode) move(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	// must be an invoke
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
-
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4, function followed by 2 names and 1 value")
-	}
-
-	A = args[1]
-	B = args[2]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Invalid transaction amount, expecting a integer value")
-	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	if transientMap, err := stub.GetTransient(); err == nil {
-		if transientData, ok := transientMap["result"]; ok {
-			return shim.Success(transientData)
-		}
-	}
-	return shim.Success(nil)
-}
-
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	A := args[1]
-
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
-	if err != nil {
-		return shim.Error("Failed to delete state")
-	}
-
-	return shim.Success(nil)
-}
-
-// Query callback representing the query of a chaincode
 func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-
-	var A string // Entities
-	var err error
-
+	fmt.Println("########### Query ###########")
 	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
 	}
 
-	A = args[1]
+	id := args[1]
 
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
+	assetBlob, err := stub.GetState(id)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error("Failed to get state")
+	}
+	if assetBlob == nil {
+		return shim.Error("Not found!")
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
+	jsonResp := "{\"id\":\"" + id + "\",\"val\":\"" + string(assetBlob) + "\"}"
 
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	a1 := Asset{}
+	json.Unmarshal([]byte(assetBlob), &a1)
+	fmt.Printf("-> %+v\n", a1)
 	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
+	return shim.Success(assetBlob)
 }
 
 func main() {
