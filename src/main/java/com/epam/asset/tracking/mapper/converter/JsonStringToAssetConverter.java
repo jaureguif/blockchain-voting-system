6bdd944132d32bfd81c1b31cdc0f45e43ae91ef5
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-13 12:29:48 -0600

    JSON to Asset test class using spring autowired

--- src/main/java/com/epam/asset/tracking/mapper/converter/JsonStringToAssetConverter.java
+++ src/main/java/com/epam/asset/tracking/mapper/converter/JsonStringToAssetConverter.java
@@ -3,12 +3,12 @@ package com.epam.asset.tracking.mapper.converter;
 import com.epam.asset.tracking.domain.Asset;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import ma.glasnost.orika.CustomConverter;
 import ma.glasnost.orika.metadata.Type;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 /**
  * Created on 1/25/2018.
@@ -19,7 +19,7 @@ public class JsonStringToAssetConverter extends CustomConverter<String, Asset>{
     /**
      * Created by Miguel Monraz on 12/02/2018
      * Converts a given JSON String into an Java Object
-     * @param entity a JSON String that represents an Asset
+     * @param source a JSON String that represents an Asset
      * @return Asset
      *
      **/
@@ -30,10 +30,13 @@ public class JsonStringToAssetConverter extends CustomConverter<String, Asset>{
 		
         ObjectMapper objectMapper = new ObjectMapper();
         
-        try {
-            asset = objectMapper.readValue(source, Asset.class);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(!StringUtils.isEmpty(source)){
+
+            try {
+                asset = objectMapper.readValue(source, Asset.class);
+            } catch (Exception e) {
+                e.getMessage();
+            }
         }
 
         return asset;
