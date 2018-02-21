886aa3531c0cafce40073b97b6c520bbb329d11c
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-21 16:09:43 -0600

    Changed to user eal username and password

--- src/main/java/com/epam/asset/tracking/service/UserServiceImpl.java
+++ src/main/java/com/epam/asset/tracking/service/UserServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.epam.asset.tracking.domain.BusinessProvider;
+import com.epam.asset.tracking.repository.BusinessProviderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,38 +16,50 @@ import org.springframework.stereotype.Service;
 
 import com.epam.asset.tracking.domain.User;
 import com.epam.asset.tracking.domain.User.Role;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @Service(value = "userService")
+
 public class UserServiceImpl implements UserDetailsService, UserService {
 	
-	User user = removeThis();
-	
-	private User removeThis() {
+	//User user = removeThis();
+
+	@Autowired
+	BusinessProviderRepository businessProviderRepository;
+
+
+	/*private User removeThis() {
 		User user = new User(Role.USER);
 		user.setUsername("Adam");
 		user.setPassword("$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu");
 		return user;
-	}
+	}*/
 	
 	@Override
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-		
-		if(userId == null || !userId.equals(user.getUsername())) {
+
+		BusinessProvider  user = businessProviderRepository.findByUsername(userId);
+
+
+		if(user == null) {
 			throw new UsernameNotFoundException("Invalid username or password.");
 		}
+
+
+
 		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthority());
 	}
 
 	
 	private List<GrantedAuthority> getAuthority() {
-		return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_BUSINESS_PROVIDER"));
 	}
 
 	@Override
 	public List<User> findAll() {
-		List<User> list = new ArrayList<>();
-		//userDao.findAll().iterator().forEachRemaining(list::add);
-		list.add(user);
-		return list;
+		throw new NotImplementedException();
 	}
+
+
+
 }
