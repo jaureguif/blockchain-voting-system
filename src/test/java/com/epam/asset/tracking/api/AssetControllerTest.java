ad6c465b823640a9fc4ebeb68fa2d2dd790a20fa
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-21 16:12:31 -0600

    To test the Asset Controller

---
+++ src/test/java/com/epam/asset/tracking/api/AssetControllerTest.java
@@ -0,0 +1,39 @@
+package com.epam.asset.tracking.api;
+
+import com.epam.asset.tracking.repository.BusinessProviderRepository;
+import com.epam.asset.tracking.service.ApiService;
+import com.epam.asset.tracking.service.BusinessProviderService;
+import com.epam.asset.tracking.service.EntityService;
+import com.epam.asset.tracking.web.AbstractWebTest;
+import ma.glasnost.orika.MapperFacade;
+import org.hamcrest.text.IsEmptyString;
+import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+public class AssetControllerTest extends AbstractWebTest {
+    @MockBean
+    ApiService api;
+
+    @MockBean
+    EntityService entity;
+
+    @MockBean
+    MapperFacade mapperFacade;
+
+    @MockBean
+    BusinessProviderService businessProviderService;
+
+    @MockBean
+    BusinessProviderRepository businessProviderRepository;
+
+    @Test
+    public void testConverter() throws Exception{
+        mockMvc.perform(get("/asset/tracking/asset/9d40ee4e-bf1e-4f74-8237-c5e9b6e8f6d3"));
+
+    }
+}
