6696ce325585739bbecc565bb4ce31249bc01f13
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-21 16:05:34 -0600

    Encrypt businessproviderPassword when adding

--- src/main/java/com/epam/asset/tracking/service/BusinessProviderServiceImpl.java
+++ src/main/java/com/epam/asset/tracking/service/BusinessProviderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.epam.asset.tracking.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.epam.asset.tracking.domain.BusinessProvider;
@@ -11,9 +12,13 @@ public class BusinessProviderServiceImpl implements BusinessProviderService{
 
 	@Autowired
 	private BusinessProviderRepository repository;
+
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Override
 	public BusinessProvider save(BusinessProvider entity) {
+		entity.setPassword(bCryptPasswordEncoder.encode(entity.getPassword()));
 		 return repository.save(entity);
 	}
 
