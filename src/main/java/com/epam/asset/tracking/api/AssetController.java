6f934026ef763ffd953210a33727429b006fd344
ulises-mancilla <mancilla.ulises@gmail.com>
2018-02-23 16:01:57 -0600

    Fix to AssetController

--- src/main/java/com/epam/asset/tracking/api/AssetController.java
+++ src/main/java/com/epam/asset/tracking/api/AssetController.java
@@ -1,14 +1,5 @@
 package com.epam.asset.tracking.api;
 
-import com.epam.asset.tracking.domain.Asset;
-import com.epam.asset.tracking.dto.AssetDTO;
-import com.epam.asset.tracking.exception.AssetNotFoundException;
-import com.epam.asset.tracking.service.ApiService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-
 import java.util.UUID;
 
 import javax.validation.Valid;
@@ -18,9 +9,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.epam.asset.tracking.domain.Asset;
+import com.epam.asset.tracking.dto.AssetDTO;
+import com.epam.asset.tracking.exception.AssetNotFoundException;
+import com.epam.asset.tracking.service.ApiService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/asset/tracking/asset")
@@ -49,8 +57,8 @@ public class AssetController {
 	@ApiResponses({@ApiResponse(code = 201, message = "Returns saved Asset", response = Asset.class),
 		@ApiResponse(code = 400, message = "Bad, request.")})
 
-	@PostMapping(consumes= {"multipart/form-data"} )
-	public AssetDTO postAsset(@RequestPart("file") MultipartFile file ) {
+	@PostMapping(value = "/", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE})
+	public AssetDTO postAsset(@RequestPart("file") MultipartFile file, @RequestPart("asset") @RequestBody AssetDTO asset ) {
 		logger.debug("Call to POST:/asset/tracking/asset");
 		
 		return new AssetDTO();
