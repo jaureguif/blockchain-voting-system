d6bc697d99b6db7fec3604198c1bca402c294f4c
Miguel_Monraz <pitfally2k@hotmail.com>
2018-02-27 16:53:46 -0600

    JSON to Asset test class using spring autowired and valid String and null String tests

--- src/test/java/com/epam/asset/tracking/mapper/converter/JsonStringToAssetConverterTest.java
+++ src/test/java/com/epam/asset/tracking/mapper/converter/JsonStringToAssetConverterTest.java
@@ -19,7 +19,7 @@ public class JsonStringToAssetConverterTest {
     @Test
     public void convertJSONStringToAsset(){
 
-        String jsonAsset = "{\"uuid\": \"9d40ee4e-bf1e-4f74-8237-c5e9b6e8f6d3\", \"serialNumbers\": \"3VW1W21KIBM312176\", \"assetType\": \"VEHICLE\", \"ownerName\": \"Jhonn Doe\", \"description\": \"2011 VW JETTA STYLE ACTIVE MANUAL TRANSMISION. SIDE AIRBAGS PACKAGE, COLOR: WHITE CANDY INTERIOR COLOR: BLACK FABRIC . ENGINE: 2.5L FIVE CYLINDERS ENGINE NUMBER: CCC094323 MADE IN: MEXICO BUYER NAME: JHONN DOE ADDRESS: 123 ABBY ROAD, THE DOMAIN. AUTIN TEXAS, USA. SELLER NAME: RAY REDDINGTON\"}";
+        String jsonAsset = "{\"uuid\": \"9d40ee4e-bf1e-4f74-8237-c5e9b6e8f6d3\", \"serialNumber\": \"3VW1W21KIBM312176\", \"assetType\": \"VEHICLE\", \"ownerName\": \"Jhonn Doe\", \"description\": \"2011 VW JETTA STYLE ACTIVE MANUAL TRANSMISION. SIDE AIRBAGS PACKAGE, COLOR: WHITE CANDY INTERIOR COLOR: BLACK FABRIC . ENGINE: 2.5L FIVE CYLINDERS ENGINE NUMBER: CCC094323 MADE IN: MEXICO BUYER NAME: JHONN DOE ADDRESS: 123 ABBY ROAD, THE DOMAIN. AUTIN TEXAS, USA. SELLER NAME: RAY REDDINGTON\"}";
 
         JsonStringToAssetConverter converter = new JsonStringToAssetConverter();
 
@@ -67,6 +67,24 @@ public class JsonStringToAssetConverterTest {
         
         //Converts the given JSON Script into an Asset Object (Expecting an exception to be thrown)
         converter.convert(jsonAsset, null);
+
+    }
+
+    @Test
+    public void convertJSONStringToAssetNullString(){
+
+        String jsonAsset = null;
+
+        JsonStringToAssetConverter converter = new JsonStringToAssetConverter();
+
+
+        Asset asset = null;
+
+        //Converts the given JSON Script into an Asset Object
+        asset  = converter.convert(jsonAsset, null);
+
+        //Check if the values from the Asset object created matched the on the JSON String
+        assertThat("Asset is not null", asset, is(nullValue()));
 
     }
     
