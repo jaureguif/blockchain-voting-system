bb2cd51b32ce97f81cb5c3b613248937c8365d38
ulises-mancilla <mancilla.ulises@gmail.com>
2018-03-21 16:31:31 -0600

    CacheConfig Refactor

--- src/main/java/com/epam/asset/tracking/config/CacheConfig.java
+++ src/main/java/com/epam/asset/tracking/config/CacheConfig.java
@@ -1,28 +1,12 @@
 package com.epam.asset.tracking.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
   
-  /*@Bean
-  public CacheManager cacheManager() {
-    return new EhCacheCacheManager(ehCacheCacheManager().getObject());
-  }
-  
-  @Bean
-  public EhCacheManagerFactoryBean ehCacheCacheManager() {
-    EhCacheManagerFactoryBean factory = new EhCacheManagerFactoryBean();
-    factory.setConfigLocation(new ClassPathResource("ehcache.xml"));
-    factory.setShared(true);
-    return factory;
-  }*/
+ 
   
 }
