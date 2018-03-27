40eca12462ba68823f1870a25248e47ec2eac93d
Miguel_Monraz <pitfally2k@hotmail.com>
2018-03-27 09:53:02 -0600

    Removed email configuration

--- src/main/java/com/epam/asset/tracking/service/UserServiceImpl.java
+++ src/main/java/com/epam/asset/tracking/service/UserServiceImpl.java
@@ -78,22 +78,4 @@ public class UserServiceImpl implements UserDetailsService, UserService {
 
   }
 
-
-  @Bean
-  public JavaMailSender getJavaMailSender() {
-    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(587);
-
-    mailSender.setUsername("blockchainepam@gmail.com");
-    mailSender.setPassword("blockchain123");
-
-    Properties props = mailSender.getJavaMailProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
-
-    return mailSender;
-  }
 }
