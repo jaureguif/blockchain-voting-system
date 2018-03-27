29c369c2fdb768702ac14b2b361e7d24f1f62f70
Miguel_Monraz <pitfally2k@hotmail.com>
2018-03-27 15:38:03 -0600

    Removed Generate Password and send email methods

--- src/main/java/com/epam/asset/tracking/service/UserServiceImpl.java
+++ src/main/java/com/epam/asset/tracking/service/UserServiceImpl.java
@@ -1,41 +1,22 @@
 package com.epam.asset.tracking.service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
-
-import com.epam.asset.tracking.domain.BusinessProvider;
-import com.epam.asset.tracking.exception.InvalidUserException;
-import org.passay.CharacterRule;
-import org.passay.EnglishCharacterData;
-import org.passay.PasswordGenerator;
+import com.epam.asset.tracking.repository.BusinessProviderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import com.epam.asset.tracking.repository.BusinessProviderRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserDetailsService, UserService {
 
   @Autowired
   BusinessProviderRepository userRepository;
-
-  @Autowired
-  JavaMailSender emailSender;
-
-  @Autowired
-  BusinessProviderService businessProviderService;
-
-  private String newPassword;
 
   @Override
   public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
@@ -49,33 +30,5 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     return Arrays.asList(new SimpleGrantedAuthority("ROLE_BUSINESS_PROVIDER"));
   }
 
-  @Override
-  public BusinessProvider generateNewPassword(String username) throws InvalidUserException {
-    PasswordGenerator passwordGenerator = new PasswordGenerator();
-
-    CharacterRule characterRule = new CharacterRule(EnglishCharacterData.Alphabetical);
-
-    newPassword = passwordGenerator.generatePassword(8, characterRule);
-
-    BusinessProvider userData = businessProviderService.findUserbyUsername(username).orElseThrow(() -> new InvalidUserException("Invalid username provided"));
-
-    businessProviderService.updatePassword(userData, newPassword);
-
-    return userData;
-
-
-  }
-
-  @Override
-  public void sendEmail(BusinessProvider user) throws InvalidUserException{
-
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(user.getEmail());
-    message.setSubject("New password Generated:");
-    message.setText("Hi "+user.getName()+"\n\nThis is your generated password: "+newPassword);
-
-    emailSender.send(message);
-
-  }
 
 }
