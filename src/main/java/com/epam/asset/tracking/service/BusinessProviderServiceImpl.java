3627e87b9e292d8e843105cb00f3b4a513d93b16
Miguel_Monraz <pitfally2k@hotmail.com>
2018-03-27 15:37:07 -0600

    Added Generate Password and send email methods

--- src/main/java/com/epam/asset/tracking/service/BusinessProviderServiceImpl.java
+++ src/main/java/com/epam/asset/tracking/service/BusinessProviderServiceImpl.java
@@ -1,6 +1,12 @@
 package com.epam.asset.tracking.service;
 
+import com.epam.asset.tracking.exception.InvalidUserException;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +23,14 @@ public class BusinessProviderServiceImpl implements BusinessProviderService {
 
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	BusinessProviderService businessProviderService;
+
+	@Autowired
+	JavaMailSender emailSender;
+
+	private String newPassword;
 
 	@Override
 	public BusinessProvider save(BusinessProvider entity) {
@@ -35,5 +49,35 @@ public class BusinessProviderServiceImpl implements BusinessProviderService {
 		entity.setPassword(bCryptPasswordEncoder.encode(password));
 		return repository.save(entity);
 	}
+
+	@Override
+	public BusinessProvider generateNewPassword(String username) throws InvalidUserException {
+		PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+		CharacterRule characterRule = new CharacterRule(EnglishCharacterData.Alphabetical);
+
+		newPassword = passwordGenerator.generatePassword(8, characterRule);
+
+		BusinessProvider userData = businessProviderService.findUserbyUsername(username).orElseThrow(() -> new InvalidUserException("Invalid username provided"));
+
+		businessProviderService.updatePassword(userData, newPassword);
+
+		return userData;
+
+
+	}
+
+	@Override
+	public void sendEmail(BusinessProvider user) {
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(user.getEmail());
+		message.setSubject("New password Generated:");
+		message.setText("Hi "+user.getName()+"\n\nThis is your generated password: "+newPassword);
+
+		emailSender.send(message);
+
+	}
+
 
 }
