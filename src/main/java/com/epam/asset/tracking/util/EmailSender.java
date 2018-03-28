cd6113e7639ec296142f6483ae61a8a084403670
Miguel_Monraz <pitfally2k@hotmail.com>
2018-03-28 12:59:51 -0600

    Create instance of JavaMailSenderImpl instead of use @Autowired

--- src/main/java/com/epam/asset/tracking/util/EmailSender.java
+++ src/main/java/com/epam/asset/tracking/util/EmailSender.java
@@ -3,15 +3,16 @@ package com.epam.asset.tracking.util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailSender {
 
-    @Autowired
-    JavaMailSender emailSender;
 
     public void sendEmail(String email, String name, String newPasword) {
+
+        JavaMailSender emailSender = new JavaMailSenderImpl();
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
