4ef0debb1478d9ada4e0cd90615470af34f83b2c
Miguel_Monraz <pitfally2k@hotmail.com>
2018-03-28 11:38:38 -0600

    Create single method for calling generate password and send email and make use of the 2 new classes created and removed unused imports

--- src/main/java/com/epam/asset/tracking/service/BusinessProviderServiceImpl.java
+++ src/main/java/com/epam/asset/tracking/service/BusinessProviderServiceImpl.java
@@ -1,17 +1,13 @@
 package com.epam.asset.tracking.service;
 
+import com.epam.asset.tracking.domain.BusinessProvider;
 import com.epam.asset.tracking.exception.InvalidUserException;
-import org.passay.CharacterRule;
-import org.passay.EnglishCharacterData;
-import org.passay.PasswordGenerator;
+import com.epam.asset.tracking.repository.BusinessProviderRepository;
+import com.epam.asset.tracking.util.EmailSender;
+import com.epam.asset.tracking.util.RandomPasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import com.epam.asset.tracking.domain.BusinessProvider;
-import com.epam.asset.tracking.repository.BusinessProviderRepository;
 
 import java.util.Optional;
 
@@ -25,12 +21,10 @@ public class BusinessProviderServiceImpl implements BusinessProviderService {
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Autowired
-	BusinessProviderService businessProviderService;
+	private EmailSender emailSender;
 
 	@Autowired
-	JavaMailSender emailSender;
-
-	private String newPassword;
+	private RandomPasswordGenerator passwordGenerator;
 
 	@Override
 	public BusinessProvider save(BusinessProvider entity) {
@@ -45,39 +39,23 @@ public class BusinessProviderServiceImpl implements BusinessProviderService {
 	}
 
 	@Override
+	public void generatePasswordAndSendEmail(String username) throws InvalidUserException {
+		String newPassword = passwordGenerator.generateNewPassword();
+
+		BusinessProvider userData = findUserbyUsername(username).orElseThrow(() -> new InvalidUserException("Invalid username provided"));
+
+		updatePassword(userData, newPassword);
+
+		emailSender.sendEmail(userData.getEmail(), userData.getName(), newPassword);
+	}
+
+	@Override
 	public BusinessProvider updatePassword(BusinessProvider entity, String password) {
 		entity.setPassword(bCryptPasswordEncoder.encode(password));
 		return repository.save(entity);
 	}
 
-	@Override
-	public BusinessProvider generateNewPassword(String username) throws InvalidUserException {
-		PasswordGenerator passwordGenerator = new PasswordGenerator();
 
-		CharacterRule characterRule = new CharacterRule(EnglishCharacterData.Alphabetical);
-
-		newPassword = passwordGenerator.generatePassword(8, characterRule);
-
-		BusinessProvider userData = businessProviderService.findUserbyUsername(username).orElseThrow(() -> new InvalidUserException("Invalid username provided"));
-
-		businessProviderService.updatePassword(userData, newPassword);
-
-		return userData;
-
-
-	}
-
-	@Override
-	public void sendEmail(BusinessProvider user) {
-
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(user.getEmail());
-		message.setSubject("New password Generated:");
-		message.setText("Hi "+user.getName()+"\n\nThis is your generated password: "+newPassword);
-
-		emailSender.send(message);
-
-	}
 
 
 }
