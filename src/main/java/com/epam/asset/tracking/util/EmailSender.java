d7f464bf75bacc8c55629415c55157360ce53880
dan-zx <danyboy_shaggy@hotmail.com>
2018-03-30 10:55:09 -0600

    Add EmailSenderTest that actually tests something :P

--- src/main/java/com/epam/asset/tracking/util/EmailSender.java
+++ src/main/java/com/epam/asset/tracking/util/EmailSender.java
@@ -8,17 +8,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailSender {
 
-    @Autowired
-    JavaMailSender emailSender;
+    private final JavaMailSender emailSender;
 
-    public void sendEmail(String email, String name, String newPasword) {
+    public @Autowired EmailSender(JavaMailSender emailSender) {
+      this.emailSender = emailSender;
+    }
 
+    public void sendEmail(String email, String name, String newPassword) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("New password Generated:");
-        message.setText("Hi "+name+"\n\nThis is your generated password: "+newPasword);
-
+        message.setText("Hi " + name + "\n\nThis is your generated password: " + newPassword);
         emailSender.send(message);
-
     }
 }
